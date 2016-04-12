165147b528920c4c3849b28e03b6058cae4a5675
gabriel kuma <gkuma@student.42.fr>
2016-04-12 03:50:00 +0200

    ex04

--- j05/ex03/jokes.ml
+++ j05/ex03/jokes.ml
@@ -10,5 +10,28 @@
 (*                                                                            *)
 (* ************************************************************************** *)
 
-let () =
+let main argv =
+	let ic = open_in (Array.get argv 1) in
+(*	let tab = Array.make 1 "" in *)
+	try
+  	while true do
+  	  let s = input_line ic in
+		print_endline s
+(*		print_endline tab.(0)
+		tab = Array.append tab s; *)
+	done
+	with End_of_file ->
+  	close_in ic
+	
+
+let () = 
+	let argv = Sys.argv in
+	if (Array.length argv) != 2 then exit (-1)
+	else main argv
        
+	(*let cmpt = ref (0) in 
+	#for i = 0 do 
+	
+	#done
+	let s = input_line ic in
+	*)
